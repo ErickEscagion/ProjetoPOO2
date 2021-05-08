0aa31ee1cecf5310160a4e725034eeca11731898
Michell Algarra <michell.algarra@gmail.com>
2021-05-08 17:30:27 -0300

    fixed constructor attributes setters

--- src/main/java/project/poo2/entities/Admin.java
+++ src/main/java/project/poo2/entities/Admin.java
@@ -55,7 +55,7 @@ public class Admin extends BaseUser {
     }
 
     public Admin(Admin admin){
-        this.phoneNumber = admin.getName();
+        this.phoneNumber = admin.getPhoneNumber();
     }
 
 }
