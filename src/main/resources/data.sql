d7c3912a36af2d600ce98ba9789e81029720dc0c
Michell Algarra <michell.algarra@gmail.com>
2021-05-08 17:32:16 -0300

    added a new insert line to base user and another to attend

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,8 +3,9 @@ INSERT INTO TB_EVENT(name, description, place, email_Contact,start_Date,end_Date
 INSERT INTO TB_EVENT(name, description, place, email_Contact,start_Date,end_Date,start_Time,end_Time) VALUES ('Evento da Laura','Descricao do Evento da Laura', 'Local do Evento da Laura','Laura@contato.com', '2021-03-25', '2021-03-25', '16:00:00', '22:30:10');
 INSERT INTO TB_EVENT(name, description, place, email_Contact,start_Date,end_Date,start_Time,end_Time) VALUES ('Event from Laura','Description from Event Laura', 'Event place from Laura','Laura@contact.com', '2021-03-25', '2021-03-25', '16:00:00', '22:30:10');
 
-
 INSERT INTO TB_BASEUSER(name, email) VALUES('erick', 'erick@gmail.com');
+INSERT INTO TB_BASEUSER(name, email) VALUES('Michell', 'michell@gmail.com');
 
 INSERT INTO TB_ADMIN(user_id, phone_number) VALUES (1, '15998181242');
--- INSERT INTO TB_ADMIN(user_id, phone_number) VALUES (2, '15988776655');
+
+INSERT INTO TB_ATTEND(user_id, balance) VALUES (2, 4000);
