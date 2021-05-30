234b4d28d592632a7b29fbc9e478d0743bc7f016
Michell Algarra <michell.algarra@gmail.com>
2021-05-30 04:21:24 -0300

    created a jpql query to get tickets by an event id

--- src/main/java/project/poo2/repositories/TicketRepository.java
+++ src/main/java/project/poo2/repositories/TicketRepository.java
@@ -1,9 +1,19 @@
 package project.poo2.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import project.poo2.entities.Ticket;
+import project.poo2.entities.TicketType;
 
 public interface TicketRepository extends JpaRepository<Ticket,Long> {
-    
+    @Query("SELECT t FROM Ticket t " +
+    "LEFT JOIN t.event e " +
+    "WHERE " +
+    "(e.id = :eventId) AND " +
+    "(:ticketType = null OR t.type = :ticketType)"
+    )
+    public Page <Ticket> findAllByEventId(Pageable pageRequest, Long eventId, TicketType ticketType);
 }
