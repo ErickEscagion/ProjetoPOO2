409828d021bda9ba0fe3daf2fa1c5dd7c4d9857d
Michell Algarra <michell.algarra@gmail.com>
2021-05-30 04:24:05 -0300

    fixed some insert values from events and tickets

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -26,7 +26,7 @@ INSERT INTO TB_PLACE(name,address) VALUES ('Ceu','Nuvens');
 
 INSERT INTO TB_EVENT(name, description, email_Contact,start_Date,end_Date,start_Time,end_Time,amount_free_tickets,amount_paid_tickets,free_tickets_selled,paid_tickets_selled,price_ticket,admin_user_id) VALUES ('Evento do Erick','Descricao do Evento do Erick', 'email@contato.com', '2021-03-10', '2021-03-11', '22:00:50', '17:35:10', 30, 270, 0, 0, 50, 1);
 INSERT INTO TB_EVENT(name, description, email_Contact,start_Date,end_Date,start_Time,end_Time,amount_free_tickets,amount_paid_tickets,free_tickets_selled,paid_tickets_selled,price_ticket,admin_user_id) VALUES ('Event from Erick','Description from Event', 'email@contact.com', '2021-03-11', '2021-03-11', '18:55:30', '22:03:12', 50, 50, 0, 0, 30, 1);
-INSERT INTO TB_EVENT(name, description, email_Contact,start_Date,end_Date,start_Time,end_Time,amount_free_tickets,amount_paid_tickets,free_tickets_selled,paid_tickets_selled,price_ticket,admin_user_id) VALUES ('Evento da Laura','Descricao do Evento da Laura', 'Laura@contato.com', '2021-03-25', '2021-03-25', '16:00:00', '22:30:10', 5, 255, 0, 0, 90, 2);
+INSERT INTO TB_EVENT(name, description, email_Contact,start_Date,end_Date,start_Time,end_Time,amount_free_tickets,amount_paid_tickets,free_tickets_selled,paid_tickets_selled,price_ticket,admin_user_id) VALUES ('Evento da Laura','Descricao do Evento da Laura', 'Laura@contato.com', '2021-03-25', '2021-03-25', '16:00:00', '22:30:10', 5, 255, 2, 2, 90, 2);
 INSERT INTO TB_EVENT(name, description, email_Contact,start_Date,end_Date,start_Time,end_Time,amount_free_tickets,amount_paid_tickets,free_tickets_selled,paid_tickets_selled,price_ticket,admin_user_id) VALUES ('Event from Laura','Description from Event Laura', 'Laura@contact.com', '2021-03-25', '2021-03-25', '16:00:00', '22:30:10', 0, 100, 0, 0, 520, 3);
 
 INSERT INTO TB_PLACE_EVENT(place_id, event_id) VALUES (1, 4);
@@ -36,5 +36,5 @@ INSERT INTO TB_PLACE_EVENT(place_id, event_id) VALUES (4, 1);
 
 INSERT INTO TB_TICKET(type, date, price, attend_user_id, event_id) VALUES (0, CURRENT_TIMESTAMP, 0, 5, 3);
 INSERT INTO TB_TICKET(type, date, price, attend_user_id, event_id) VALUES (0, CURRENT_TIMESTAMP, 0, 5, 3);
-INSERT INTO TB_TICKET(type, date, price, attend_user_id, event_id) VALUES (1, CURRENT_TIMESTAMP, 50, 6, 3);
-INSERT INTO TB_TICKET(type, date, price, attend_user_id, event_id) VALUES (1, CURRENT_TIMESTAMP, 100, 7, 3);
+INSERT INTO TB_TICKET(type, date, price, attend_user_id, event_id) VALUES (1, CURRENT_TIMESTAMP, 90, 6, 3);
+INSERT INTO TB_TICKET(type, date, price, attend_user_id, event_id) VALUES (1, CURRENT_TIMESTAMP, 90, 7, 3);
